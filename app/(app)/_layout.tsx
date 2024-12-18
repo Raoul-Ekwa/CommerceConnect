bbca388d1b2e7ac1fa1e9261f4da65fc7c6115ff
Raoul-Ekwa <tempeteekwa1995@gmail.com>
2024-12-18 11:41:56 +0100

    first version of the App

---
+++ app/(app)/_layout.tsx
@@ -0,0 +1,14 @@
+import { StyleSheet, Text, View } from 'react-native'
+import React from 'react'
+
+const appLayout = () => {
+  return (
+    <View>
+      <Text>Appeller depuis (app)</Text>
+    </View>
+  )
+}
+
+export default appLayout
+
+const styles = StyleSheet.create({})
